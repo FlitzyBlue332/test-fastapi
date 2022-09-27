be3966ce4c4862671f7c15a617e5b85da190fb7f
FlitzyBlue332 <raihanaulia332@gmail.com>
2022-09-28 04:22:11 +0700

    Final Change.. WRYYYYYY

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+#ganti env: .\env\Scripts\Activate.ps1  
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"18220031": "Muhammad Raihan Aulia"}
+class Mahasiswa(BaseModel):
+    nim: str
+    name: str
+
+daftarMahasiswa = {"18220031":"Muhammad Raihan Aulia"}
+
+@app.post("/")
+async def create_mahasiswa(mahasiswa: Mahasiswa):
+    daftarMahasiswa[mahasiswa.nim] = mahasiswa.name
+    return mahasiswa
+
+@app.get("/mahasiswa/{nim}")
+async def read_mahasiswa(nim):
+    return {nim : daftarMahasiswa[nim]}
